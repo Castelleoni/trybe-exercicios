eac10b8c1154c5b176b679a9a3d9ede76f866752
Alexandre Castelleoni <a.castelleoni@gmail.com>
2022-10-20 19:08:01 -0300

    ex 01

---
+++ Modulo-01-Fundamentos/secao-04-javascript/dia-4.1/ex-01/ex001.js
@@ -0,0 +1,8 @@
+const myName = "Alexandre";
+const birthCity = "São Paulo"
+let birthYear = 1997;
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+birthYear = 2030;
+console.log(birthYear);
