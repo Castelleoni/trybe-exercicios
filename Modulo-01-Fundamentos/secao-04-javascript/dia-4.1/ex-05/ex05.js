46ac508c34df780b7992364dfbcdc47f1be1baaa
Alexandre Castelleoni <a.castelleoni@gmail.com>
2022-10-21 04:04:59 -0300

    Ex5

---
+++ Modulo-01-Fundamentos/secao-04-javascript/dia-4.1/ex-05/ex05.js
@@ -0,0 +1,9 @@
+let weekDay = "Domingo"
+
+if (weekDay == "Domingo" || weekDay == "Sabado")
+{
+    console.log("Oba fim de semana")
+}
+else {
+    console.log ("Oba, mais um dia de aprendizado na Trybe >:D")
+}
