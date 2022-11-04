014fda063f9d05cf80e39af65b0e0bceddd92fb3
Alexandre Castelleoni <a.castelleoni@gmail.com>
2022-11-04 13:40:02 -0300

    tentando realizar o exercicicos do dia 4

---
+++ Modulo-01-Fundamentos/secao-05-DOM-Eventos-e-Web-Storage/dia-5.4/Ex-dia/script.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+    let select = document.querySelector('select');
+    select.addEventListener('change', function(){
+        let selecionado = select.selectedOptions[0];
+        document.body.style.backgroundColor = selecionado.value;
+    })
+
+
+
+}
